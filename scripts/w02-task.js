38466f282b4bc03a81dee0642be39c1e42f57cc2
nefiserrano <nefi_3902@hotmail.com>
2024-01-20 14:55:37 -0600

    Fixed

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -7,25 +7,17 @@ let fullName = "Nefi Raul Serrano Ramirez";
 let currentYear = "2024";
 let profilePicture = "images/Foto.jpg";
 
-
-
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
 const imageElement = document.querySelector("img");
 
-
-
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 imageElement.setAttribute("src", profilePicture);
 imageElement.setAttribute("alt",  `Profile image of ${fullName}`);
-
-
-
-
 
 /* Step 5 - Array */
 const favoriteFoods = ["Pizza", "Tacos", "Enchiladas", "Cheeseburgers", "French Fries"];
@@ -37,7 +29,3 @@ favoriteFoods.shift();
 foodElement.innerHTML += `<br>${favoriteFoods}`;
 favoriteFoods.pop();
 foodElement.innerHTML += `<br>${favoriteFoods}`;
-
-
-
-
